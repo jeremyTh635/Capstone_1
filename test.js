6a90d6f0424a8ca0ca7820fc373c650c46c0c6c7
Jeremy Thorpe <tpyth0n@hotmail.com>
2024-12-05 11:48:01 +0000

    more changes

--- test.js
+++ test.js
@@ -1,3 +1,29 @@
-function getMeals() {
+let orders = [];
+let favouriteMeals = [];
+let chefsFavourites = [];
+
+function pageLoad() {
+  if (sessionStorage.getItem("hasCodeRunBefore") === null) {
+    sessionStorage.setItem("hasCodeRunBefore", true);
+  } else {
+    orders = JSON.parse(sessionStorage.getItem("orders"));
+  }
+
+  function Orders(orderList, orderNo, completed) {
+    this.orderList = orderList;
+    this.orderNo = orderNo;
+    this.completed = completed;
+  }
+
+  function getIngredient() {
+    let ingredient = prompt("What is the main ingredient of your meal?").toLowerCase();
+  if (ingredient.includes(" ")) {
+      ingredient = ingredient.replaceAll(" ", "_");
+    }
+    return ingredient;
+  }
+
+  let mainIngredient = getIngredient();
+  console.log(mainIngredient);
 
 }
