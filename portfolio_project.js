b8eab1833508f8f6c6421941635e2b6fb264459c
Jeremy Thorpe <tpyth0n@hotmail.com>
2024-12-09 12:02:13 +0000

    Some files deleted. Others updated.

--- portfolio_project.js
+++ portfolio_project.js
@@ -4,56 +4,88 @@ let chefsFavourites = [];
 
 function pageLoad() {
 
-  if (sessionStorage.getItem("hasCodeRunBefore") === null) {
-    sessionStorage.setItem("hasCodeRunBefore", true);
+  if (sessionStorage.getItem("orderNumber") === null || sessionStorage.getItem("orders") === null) {
+    sessionStorage.setItem("orders", JSON.stringify(orders));
+    sessionStorage.setItem("orderNumber", 1)
   } else {
     orders = JSON.parse(sessionStorage.getItem("orders"));
+
   }
 
-  function Orders(orderList, orderNo, completed) {
-    this.orderList = orderList;
+  function Orders(description, orderNo, completed) {
+    this.description = description;
     this.orderNo = orderNo;
     this.completed = completed;
   }
 
   function getIngredient() {
     let ingredient = prompt("What is the main ingredient of your meal?").toLowerCase();
-  if (ingredient.includes(" ")) {
-      ingredient = mainIngredient.replaceAll(" ", "_");
+    if (ingredient.includes(" ")) {
+        ingredient = ingredient.replaceAll(" ", "_");
+      }
+      return ingredient;
     }
-    return ingredient;
-  }
 
-  let mainIngredient = getIngredient();
-  console.log(mainIngredient);
+    let mainIngredient = getIngredient();
+    console.log(mainIngredient);
 
-  function getMeals() {
-    try {
-      fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${mainIngredient}`)
-      .then((res) => res.json())
-      .then((result) => {
-      favouriteMeals = result;
-      mealsArray = favouriteMeals.meals;
-      console.log(mealsArray);
-      if (mealsArray === null) {
-        alert("Your ingredient is not available.");
-        mainIngredient = getIngredient();
-        getMeals(mainIngredient);
-      }
-      return mealsArray;
+    function getMeals() {
+      try {
+        return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${mainIngredient}`)
+        .then((res) => res.json())
+        .then((result) => {
+        favouriteMeals = result;
+        mealsArray = favouriteMeals.meals;
+        if (mealsArray === null) {
+          alert("Your ingredient is not available.");
+          mainIngredient = getIngredient();
+          getMeals(mainIngredient);
+        }
+        return mealsArray;
+        })
+        }
+        catch (err) {
+          console.log("Error fetching API", err);
+          return Promise.reject(err)
+        }
+    }
+
+    const createOrders = (fn) => {
+      fn()
+      .then((meals) => {
+        if (meals !== null) {
+          let order = new Orders();
+          order.description = meals[Math.floor(Math.random() * meals.length)].strMeal;
+          order.orderNo = orders.length + 1;
+          order.completed = Boolean(prompt("Is this order completed? Type Y or leave it blank if not completed."))
+          orders.push(order);
+          let answer = prompt("Do you want to make another order? Type Y or N");
+          if (answer === "Y") {
+            mainIngredient = getIngredient();
+            getMeals(mainIngredient);
+            createOrders(getMeals);
+          } else {
+             sessionStorage.setItem("orders", JSON.stringify(orders));
+             sessionStorage.setItem("orderNumber", orders.length + 1);
+          }
+        }
       })
-      }
-      catch (err) {
-        console.log("Error fetching API", err);
-      }
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+
+    const userOrders = createOrders(getMeals);
+
+    const orderString = sessionStorage.getItem("orders");
+    const orderObjects = JSON.parse(orderString);
+    const uncompleted = orderObjects.filter((item) => item.completed === false);
+
+    console.log(uncompleted);
+
   }
 
-  console.log(getMeals());
-  // Returns 'undefined'. Using other methods returns Promise { <pending> }
 
-  }
-
-/* The getMeals function now seems to be doing what it's supposed to do. The problem I'm now having is using the data outside the function other than logging it to the console. */
 
 
 pageLoad();
