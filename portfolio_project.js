239654184b612c140259503bf61cbaec8d103e27
Jeremy Thorpe <tpyth0n@hotmail.com>
2024-12-12 17:56:31 +0000

    Added major changes

--- portfolio_project.js
+++ portfolio_project.js
@@ -1,91 +1,143 @@
+// Declare arrays to store user's orders and data from API
 let orders = [];
 let favouriteMeals = [];
-let chefsFavourites = [];
 
-function pageLoad() {
+// Check if session storage already populated
+if (
+  sessionStorage.getItem("orderNumber") === null ||
+  sessionStorage.getItem("orders") === null
+) {
+  sessionStorage.setItem("orders", JSON.stringify(orders));
+  sessionStorage.setItem("orderNumber", 1);
+} else {
+  orders = JSON.parse(sessionStorage.getItem("orders"));
+}
 
-  if (sessionStorage.getItem("orderNumber") === null || sessionStorage.getItem("orders") === null) {
-    sessionStorage.setItem("orders", JSON.stringify(orders));
-    sessionStorage.setItem("orderNumber", 1)
-  } else {
-    orders = JSON.parse(sessionStorage.getItem("orders"));
+// Constructor function for user's order objects
+function Orders(description, orderNo, completed) {
+  this.description = description;
+  this.orderNo = orderNo;
+  this.completed = completed;
+}
 
+// Function to create list of uncompleted orders in prompt
+let str = ``;
+
+function uncompletedList(array) {
+  for (let i = 0; i < array.length; i++) {
+    let newStr = `Description:  ${array[i].description}  |  Order Number:  ${array[i].orderNo}\n`;
+    str = str + newStr;
   }
+  return str;
+}
 
-  function Orders(description, orderNo, completed) {
-    this.description = description;
-    this.orderNo = orderNo;
-    this.completed = completed;
+// Function to create user ingredient for API call
+function getIngredient() {
+  let ingredient = prompt(
+    "What is the main ingredient of your meal?"
+  ).toLowerCase();
+  if (ingredient.includes(" ")) {
+    ingredient = ingredient.replaceAll(" ", "_");
   }
+  return ingredient;
+}
 
-  function getIngredient() {
-    let ingredient = prompt("What is the main ingredient of your meal?").toLowerCase();
-    if (ingredient.includes(" ")) {
-        ingredient = ingredient.replaceAll(" ", "_");
-      }
-      return ingredient;
-    }
+// Variable to call getIngredient function and retrieve API data
+let mainIngredient = getIngredient();
 
-    let mainIngredient = getIngredient();
-    console.log(mainIngredient);
-
-    function getMeals() {
-      try {
-        return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${mainIngredient}`)
-        .then((res) => res.json())
-        .then((result) => {
+// Function to get data from API
+function getMeals() {
+  try {
+    return fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${mainIngredient}`
+    )
+      .then((res) => res.json())
+      .then((result) => {
         favouriteMeals = result;
         mealsArray = favouriteMeals.meals;
-        if (mealsArray === null) {
-          alert("Your ingredient is not available.");
-          mainIngredient = getIngredient();
-          getMeals(mainIngredient);
-        }
         return mealsArray;
-        })
-        }
-        catch (err) {
-          console.log("Error fetching API", err);
-          return Promise.reject(err)
-        }
-    }
+      });
+  } catch (err) {
+    console.log("Error fetching API", err);
+  }
+}
 
-    const createOrders = (fn) => {
-      fn()
-      .then((meals) => {
-        if (meals !== null) {
-          let order = new Orders();
-          order.description = meals[Math.floor(Math.random() * meals.length)].strMeal;
-          order.orderNo = orders.length + 1;
-          order.completed = Boolean(prompt("Is this order completed? Type Y or leave it blank if not completed."))
-          orders.push(order);
-          let answer = prompt("Do you want to make another order? Type Y or N");
-          if (answer === "Y") {
-            mainIngredient = getIngredient();
-            getMeals(mainIngredient);
-            createOrders(getMeals);
-          } else {
-             sessionStorage.setItem("orders", JSON.stringify(orders));
-             sessionStorage.setItem("orderNumber", orders.length + 1);
+// Call getMeals function
+getMeals();
+
+/* Function to create order objects from API data, pass them to session storage and update completed status */
+const createOrders = (fn) => {
+  // Get asynchronous data
+  fn()
+    .then((meals) => {
+
+      // Create order objects and push to orders array
+      let order = new Orders();
+      order.description =
+        meals[Math.floor(Math.random() * meals.length)].strMeal;
+      order.orderNo = orders.length + 1;
+      order.completed = Boolean(
+        prompt(
+          "Is this order completed? Type Y or leave it blank if not completed."
+        )
+      );
+      orders.push(order);
+
+      // Code to repeat above process to create additional order objects
+      let answer = prompt("Do you want to make another order? Type Y or N");
+      if (answer === "Y") {
+        mainIngredient = getIngredient();
+        getMeals(mainIngredient);
+        createOrders(getMeals);
+      } else {
+        // Pass orders to session storage when user has finished making orders
+        // Save number of final order to session storage
+        sessionStorage.setItem("orders", JSON.stringify(orders));
+        sessionStorage.setItem("orderNumber", orders.length);
+
+        // Get items from session storage
+        const orderString = sessionStorage.getItem("orders");
+        const orderObjects = JSON.parse(orderString);
+
+        // Filter uncompleted orders
+        const uncompleted = orderObjects.filter(
+          (item) => item.completed === false
+        );
+
+        // Display uncompleted orders to user and ask if they want to complete them
+        let userChoice = prompt(
+          `Here is a list of uncompleted orders:\n\n${uncompletedList(
+            uncompleted
+          )}\n\nDo you wish to mark these as completed? Type 1 for yes and 0 for no.`
+        );
+        // Nested loop to enable users to enable user to update completed status if they so wish
+        if (userChoice === "1") {
+          for (let i = 0; i < uncompleted.length; i++) {
+            for (let j = 0; j < orderObjects.length; j++) {
+              if (orderObjects[j].orderNo === uncompleted[i].orderNo) {
+                orderObjects[j].completed = !!Number(
+                  prompt(
+                    `Do you want to mark ${orderObjects[j].description}  ${orderObjects[j].orderNo} as completed? Type ${orderObjects[j].orderNo} for yes and 0 for no.`
+                  )
+                );
+              }
+              // Save updated order array and final order number to session storage
+              sessionStorage.setItem("orders", JSON.stringify(orderObjects));
+              sessionStorage.setItem("orderNumber", orderObjects.length);
+            }
           }
         }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    }
+      }
+    })
+    // Catch block contains code deal with event of API returning {"meals":null}
+    .catch((err) => {
+      console.log(err);
+      alert("Your ingredient is not available.");
+      mainIngredient = getIngredient();
+      getMeals();
+      createOrders(getMeals);
+    });
+};
 
-    const userOrders = createOrders(getMeals);
-
-    const orderString = sessionStorage.getItem("orders");
-    const orderObjects = JSON.parse(orderString);
-    const uncompleted = orderObjects.filter((item) => item.completed === false);
-
-    console.log(uncompleted);
-
-  }
-
-
-
-
-pageLoad();
+// Function call to createOrders
+const userOrders = createOrders(getMeals);
